63b46a6876360f9c3dfd75af1ec90e2dd467b86c
Lucas Henrique de Barros <128721960+Lucshb@users.noreply.github.com>
2024-07-17 10:59:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, s
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import pandas as pd
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.secret_key = 'secretKey'
