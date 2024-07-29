7642934f20a97d26a3b49342a41b1e248c499c09
Lucas Henrique de Barros <128721960+Lucshb@users.noreply.github.com>
2024-07-29 09:28:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import sqlite3
 import io
+import shutil
 from flask import Flask, render_template, request, redirect, url_for, session, send_file
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import pandas as pd
 import xlsxwriter
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 
 app = Flask(__name__)
@@ -16,6 +17,10 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 DATABASE = os.path.join(os.getcwd(), 'logs.db')
+BACKUP_FOLDER = os.path.join(os.getcwd(), 'backups')
+
+if not os.path.exists(BACKUP_FOLDER):
+    os.makedirs(BACKUP_FOLDER)
 
 def get_db():
     conn = sqlite3.connect(DATABASE)
@@ -76,6 +81,12 @@ def log_user_activity(user_email, action):
     cursor.execute('INSERT INTO user_logs (email, action, timestamp) VALUES (?, ?, ?)', (user_email, action, now))
     db.commit()
 
+def backup_logs():
+    now = datetime.now(pytz.timezone('America/Sao_Paulo'))
+    backup_file = os.path.join(BACKUP_FOLDER, f'logs_backup_{now.strftime("%Y%m%d_%H%M%S")}.db')
+    shutil.copy(DATABASE, backup_file)
+    print(f'Backup created: {backup_file}')
+
 @app.route('/')
 def index():
     return redirect(url_for('login'))
@@ -104,7 +115,7 @@ def login():
 def dashboard():
     all_dashboards = [
         {"url": "https://app.powerbi.com/view?r=eyJrIjoiMmU1MTBmYTItMmY3MS00NjYzLTg3ZWUtOWQyYzI1YTgyYTQxIiwidCI6ImNjMmE5NWVhLTMzNWMtNDQzYi04NDQzLWU5YWQzM2ZmOWUwNCJ9", "title": "Central de BIs"},
-        {"url": "https://app.powerbi.com/view?r=eyJrIjoiNTBjZGYxZDMtOGUzNy00OTBjLWI1NTQtNjAxM2QyOWMzMDkyIiwidCI6ImNjMmE5NWVhLTMzNWMtNDQzYi04NDQzLWU5YWQzM2ZmOWUwNCJ9", "title": "Faturamento"},
+        {"url": "https://app.powerbi.com/reportEmbed?reportId=196b835a-4b66-4f9d-b7e0-1d63d4f02e88&autoAuth=true&ctid=cc2a95ea-335c-443b-8443-e9ad33ff9e04", "title": "Faturamento"},
         {"url": "https://app.powerbi.com/view?r=eyJrIjoiMTljYjYxOGQtNDMzMy00MTE2LTkxMzYtNmZhMGM1MmMzZjgxIiwidCI6ImNjMmE5NWVhLTMzNWMtNDQzYi04NDQzLWU5YWQzM2ZmOWUwNCJ9", "title": "Controladoria Anderson"},
         {"url": "https://app.powerbi.com/view?r=eyJrIjoiYTIyNGRkZjUtYTBkMS00ZjgxLTgyOWMtOTcxYTc4NjRiMDQ2IiwidCI6ImNjMmE5NWVhLTMzNWMtNDQzYi04NDQzLWU5YWQzM2ZmOWUwNCJ9", "title": "Controladoria Luiz"},
         {"url": "https://app.powerbi.com/view?r=eyJrIjoiMWMxMTEwZWEtODM4ZS00YmM0LThjNzEtMTdkYmUwYWYzODE4IiwidCI6ImNjMmE5NWVhLTMzNWMtNDQzYi04NDQzLWU5YWQzM2ZmOWUwNCJ9", "title": "Controladoria Cesar"},
@@ -125,7 +136,7 @@ def dashboard():
         for user_db in current_user.dashboards:
             if user_db.strip() in db['url']:
                 user_dashboards.append(db)
-
+    
     print(f"Current user role: {current_user.role}")
     return render_template('dashboard.html', user_dashboards=user_dashboards, user_name=current_user.name, user_role=current_user.role)
 
@@ -180,5 +191,8 @@ def download_logs():
     return 'Access denied', 403
 
 if __name__ == '__main__':
+    # Configurar backup regular dos logs
+    backup_logs()  # Executa um backup na inicialização
+    app.before_first_request(backup_logs)  # Configura um backup antes da primeira requisição
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
